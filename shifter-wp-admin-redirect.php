8e1618b57b50f0ab852120319b407b2dde75fb37
Daniel Olson <emaildano@gmail.com>
2019-04-01 17:41:47 -0400

    testing

--- shifter-wp-admin-redirect.php
+++ shifter-wp-admin-redirect.php
@@ -19,22 +19,33 @@ if (!class_exists('Shifter_API')) {
   return;
 }
 
-define('SERVICE_TYPE', env('SERVICE_TYPE') ?: 'false');
 
-function wp_login_page() {
-  $ABSPATH = str_replace(array('\\','/'), DIRECTORY_SEPARATOR, ABSPATH);
-  return ((in_array($ABSPATH.'wp-login.php', get_included_files()) || in_array($ABSPATH.'wp-register.php', get_included_files()) ) || (isset($_GLOBALS['pagenow']) && $GLOBALS['pagenow'] === 'wp-login.php') || $_SERVER['PHP_SELF']== '/wp-login.php');
-}
+// define('SERVICE_TYPE', env('SERVICE_TYPE') ?: 'false');
 
-function shifter_wp_dashboard_redirect() {
-  include( ABSPATH . 'wp-includes/pluggable.php'); 
-  include( ABSPATH . 'wp-load.php');
-  $api = new Shifter_API;
-  if ( wp_login_page() ) {
-    wp_redirect( $api->shifter_dashboard_url, 301 ); exit;
-  }
-}
-
-// if ( SERVICE_TYPE === 'generator' ) {
-  shifter_wp_dashboard_redirect();
+// function wp_login_page() {
+//   $ABSPATH = str_replace(array('\\','/'), DIRECTORY_SEPARATOR, ABSPATH);
+//   return ((in_array($ABSPATH.'wp-login.php', get_included_files()) || in_array($ABSPATH.'wp-register.php', get_included_files()) ) || (isset($_GLOBALS['pagenow']) && $GLOBALS['pagenow'] === 'wp-login.php') || $_SERVER['PHP_SELF']== '/wp-login.php');
 // }
+
+// function shifter_wp_dashboard_redirect() {
+//   include( ABSPATH . 'wp-includes/pluggable.php'); 
+//   include( ABSPATH . 'wp-load.php');
+//   $api = new Shifter_API;
+//   if ( wp_login_page() ) {
+//     wp_redirect( $api->shifter_dashboard_url, 301 ); exit;
+//   }
+// }
+
+// // if ( SERVICE_TYPE === 'generator' ) {
+//   shifter_wp_dashboard_redirect();
+// // }
+
+function my_custom_js() {
+  echo '<script type="text/javascript">
+  jQuery(document).ready(function () {
+    if(window.location.href.indexOf("wp-admin") > -1) {
+       alert("hello, there");
+    }
+  });
+  </script>';
+}
